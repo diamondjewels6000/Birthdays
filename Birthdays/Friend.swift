fccb2e2f926c256a3dfd5331068b88c239ff9d5f
Grace Shen <graceshen6439@gmaiil.com>
2025-07-11 11:34:01 -0400

    added friend class and its able to add birthdays and name but not remove

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Grace Shen on 2025-07-11.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
