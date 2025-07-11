230442c38ee9bd329aaf02f4af1de6ea8993904e
Grace Shen <graceshen6439@gmaiil.com>
2025-07-11 10:10:35 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Grace Shen on 2025-07-11.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
